b8221f04f36edacb453db49b498da731237d07b4
julianort11 <ort.na140@gmail.com>
2025-07-16 12:13:16 -0500

    feat: :sparkles: clase chismosa completada

---
+++ classes/Chismosa.js
@@ -0,0 +1,28 @@
+class Chismosa {
+    #reputacion;
+    #nivelChisme;
+    constructor (nombre, reputacion, nivelChisme){
+        if (this.constructor === Chismosa) {
+            throw new Error ("No es posible crear un objeto de tipo Chismosa")
+        }
+        this.nombre = nombre;
+        this.#reputacion = reputacion;
+        this.#nivelChisme = nivelChisme;
+    }
+
+    recolectarInfo() {
+        throw new Error("Este método no tiene implementación porque es abstracto!!")
+    }
+
+    contarChisme() {
+        throw new Error("Este método no tiene implementación porque es abstracto!!")
+    }
+
+    getReputacion() {
+        return this.#reputacion;
+    }
+
+    getNivelChisme() {
+        return this.#nivelChisme;
+    }
+}
